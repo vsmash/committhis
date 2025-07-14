77043c51b73df11cf6771d18b83ad9d8d566e9b9
vsmash <670252+vsmash@users.noreply.github.com>
2025-07-14 13:55:16 +1000

    Bumped version to 4.8.5

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# 🫏 MAIASS v4.8.4
+# 🫏 MAIASS v4.8.5
 ## Modular AI-Assisted Semantic Savant
 
 ---
--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# MAIASS Installation Script v4.8.4
+# MAIASS Installation Script v4.8.5
 # Installs maiass.sh to a system location and creates a symlink in PATH
 
 set -e
