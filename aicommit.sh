486c16321d1aa61ce9e3d095a02b90074dcde0df
vsmash <670252+vsmash@users.noreply.github.com>
2025-07-15 13:44:07 +1000

    VEL-45 Add version option to script - feat: added `-v`/`--version` flag to display script version using `maiass --aicommit-version`

--- aicommit.sh
+++ aicommit.sh
@@ -5,6 +5,11 @@ for arg in "$@"; do
       maiass --aicommit-help
       exit 0
       ;;
+    -v|--version)
+      maiass --aicommit-version      # Try to read version from package.json in script directory
+      exit 0
+      ;;
+
   esac
 done
 
