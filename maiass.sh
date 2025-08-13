820e0c120abd554d4ce394e0d2848f141bfcfaa2
vsmash <670252+vsmash@users.noreply.github.com>
2025-08-13 17:59:13 +1000

    Bumped version to 5.7.0

--- maiass.sh
+++ maiass.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # ---------------------------------------------------------------
-# MAIASS (Modular AI-Augmented Semantic Scribe) v5.6.30
+# MAIASS (Modular AI-Augmented Semantic Scribe) v5.7.0
 # Intelligent Git workflow automation script
 # Copyright (c) 2025 Velvary Pty Ltd
 # All rights reserved.
